09c40f8d34f3b2660e6fbc27a30fdfc53ebc44ff
Rith Samnang <272400168+SMG47k@users.noreply.github.com>
2026-05-10 00:40:47 +0700

    Update settings.py

--- posdb/posdb/settings.py
+++ posdb/posdb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z%(vl@y0-dz(h8f7g)kf0%()hjf_f)=!9*qocpm)$&!zf^c6*4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['rithsamnang.pythonanywhere.com']
 
